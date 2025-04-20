451b43c1f7af695f9997281c9bb6b04cbcc4dd71
saerelmasri <saer1890@gmail.com>
2025-04-20 14:05:11 +0300

    .

--- app/api/notion/[id]/route.ts
+++ app/api/notion/[id]/route.ts
@@ -3,9 +3,9 @@ import { getPage } from "@/lib/notion";
 
 export const GET = async (
   request: NextRequest,
-  { params }: { params: { id: string } }
-): Promise<NextResponse> => {
-  const { id } = params;
+  { params }: { params: Promise<{ id: string }> }
+) => {
+  const { id } = await params; // Await required
 
   if (!id) {
     return NextResponse.json(
@@ -18,9 +18,8 @@ export const GET = async (
     const pageContent = await getPage(id);
     return NextResponse.json(pageContent, { status: 200 });
   } catch (error) {
-    console.error("Error fetching Notion page:", error);
     return NextResponse.json(
-      { message: "Internal Server Error" },
+      { message: "Internal Server Error", error },
       { status: 500 }
     );
   }
